c31406e5b8133e2823e2d311b8f34c34e542aa29
Dariusz Jackowski <dariusz.jackowski@gmail.com>
2019-12-10 22:18:24 +0100

    added host, various corrections

---
+++ DollarHost/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using WcfDollarLibrary;
+
+
+namespace DollarHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri address = new Uri("http://localhost:8733/Dollar/");
+
+            // Step 2: Create a ServiceHost instance.
+            ServiceHost host = new ServiceHost(typeof(DollarConverter), address);
+
+            try
+            {
+ 
+                host.AddServiceEndpoint(typeof(IDollar), new WSHttpBinding(), "CalculatorService");
+
+
+                ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+                behavior.HttpGetEnabled = true;
+                host.Description.Behaviors.Add(behavior);
+
+
+                host.Open();
+                Console.WriteLine("Listening...");
+
+                Console.WriteLine("Press any key to stop.");
+
+                Console.ReadKey();
+                host.Close();
+            }
+            catch (CommunicationException ce)
+            {
+
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                host.Abort();
+            }
+        }
+    }
+}
